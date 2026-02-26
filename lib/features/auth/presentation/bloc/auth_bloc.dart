c4f69f7a63d4d0d7dad06035f1fe139ad6408fa5
toumaniaboubacarbamba <toumani.bamba@epitech.eu>
2026-02-26 10:49:54 +0000

    feat: add AuthBloc

---
+++ lib/features/auth/presentation/bloc/auth_bloc.dart
@@ -0,0 +1,5 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:rotten_tomatoes/features/auth/presentation/bloc/auth_event.dart';
+import 'package:rotten_tomatoes/features/auth/presentation/bloc/auth_state.dart';
+
+class AuthBloc extends Bloc<AuthEvent, AuthState> {}

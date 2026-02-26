8dbe552091fe5f3a074277484f3b5746ea08227d
toumaniaboubacarbamba <toumani.bamba@epitech.eu>
2026-02-26 10:31:40 +0000

    feat: add AuthRepositoryImpl

---
+++ lib/features/auth/data/repositories/auth_repository_impl.dart
@@ -0,0 +1,41 @@
+import 'package:dartz/dartz.dart';
+import '../../../../core/error/failures.dart';
+import '../../domain/entities/user.dart';
+import '../../domain/repositories/auth_repository.dart';
+import '../datasources/auth_remote_data_source.dart';
+
+class AuthRepositoryImpl implements AuthRepository {
+  final AuthRemoteDataSource remoteDataSource;
+
+  AuthRepositoryImpl(this.remoteDataSource);
+
+  @override
+  Future<Either<Failure, User>> login(String email, String password) async {
+    try {
+      final user = await remoteDataSource.login(email, password);
+      return Right(user);
+    } catch (e) {
+      return Left(ServerFailure(e.toString()));
+    }
+  }
+
+  @override
+  Future<Either<Failure, Unit>> logout() async {
+    try {
+      await remoteDataSource.logout();
+      return const Right(unit);
+    } catch (e) {
+      return Left(ServerFailure(e.toString()));
+    }
+  }
+
+  @override
+  Future<Either<Failure, User>> getCachedUser() async {
+    try {
+      final user = await remoteDataSource.getCachedUser();
+      return Right(user);
+    } catch (e) {
+      return Left(CacheFailure('Aucun utilisateur connecté'));
+    }
+  }
+}

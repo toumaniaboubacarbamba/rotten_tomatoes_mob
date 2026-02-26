4ef7ec8abaed23213177d602625dfe950918ff6a
toumaniaboubacarbamba <toumani.bamba@epitech.eu>
2026-02-26 10:04:43 +0000

    feat: update UserModel with fromJson and toJson

--- lib/features/auth/data/models/user_model.dart
+++ lib/features/auth/data/models/user_model.dart
@@ -3,20 +3,22 @@ import 'package:rotten_tomatoes/features/auth/domain/entities/user.dart';
 class UserModel extends User {
   const UserModel({
     required super.id,
-    required super.username,
+    required super.email,
+    required super.name,
     required super.token,
   });
 
   factory UserModel.fromJson(Map<String, dynamic> json) {
     return UserModel(
       id: json['id'],
-      username: json['username'],
+      email: json['email'],
+      name: json['name'],
       token: json['token'],
     );
   }
 
   // Convertit le UserModel en JSON pour le stockage local
   Map<String, dynamic> toJson() {
-    return {'id': id, 'username': username, 'token': token};
+    return {'id': id, 'email': email, 'name': name, 'token': token};
   }
 }
--- lib/features/auth/domain/entities/user.dart
+++ lib/features/auth/domain/entities/user.dart
@@ -2,10 +2,16 @@ import 'package:equatable/equatable.dart';
 
 class User extends Equatable {
   final String id;
-  final String username;
+  final String email;
+  final String name;
   final String token;
 
-  const User({required this.id, required this.username, required this.token});
+  const User({
+    required this.id,
+    required this.email,
+    required this.name,
+    required this.token,
+  });
 
   @override
   List<Object?> get props => [id];

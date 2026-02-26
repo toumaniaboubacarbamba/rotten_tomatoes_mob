6a6fae8730799177763c5d6fe56273ccaf6b8795
toumaniaboubacarbamba <toumani.bamba@epitech.eu>
2026-02-26 10:40:31 +0000

    feat: add AuthEvent (Login,Logout, CheckAuth)

--- lib/features/auth/presentation/bloc/auth_event.dart
+++ lib/features/auth/presentation/bloc/auth_event.dart
@@ -6,3 +6,20 @@ abstract class AuthEvent extends Equatable {
   @override
   List<Object?> get props => [];
 }
+
+//Event pour vérifier si l'utilisateur est déjà connecté au lancement de l'application
+class AuthCheckRequested extends AuthEvent {}
+
+//Event pour tenter de connecter l'utilisateur avec un email et mot de passe
+class LoginRequested extends AuthEvent {
+  final String email;
+  final String password;
+
+  const LoginRequested({required this.email, required this.password});
+
+  @override
+  List<Object?> get props => [email, password];
+}
+
+//Event pour déconnecter l'utilisateur
+class LogoutRequested extends AuthEvent {}
